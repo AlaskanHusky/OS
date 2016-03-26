ec39373a6a288295768d67027f0bf54a8e4009c6
Pavel Yanushevsky <AlaskanHusky@users.noreply.github.com>
2016-03-26 09:14:05 +0300

    Create Program.cs

---
+++ Mutual_Exclusion_Forms/Program.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Mutual_Exclusion_Forms
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+    }
+}
